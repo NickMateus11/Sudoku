ebe39dc4affe9b1d6c99d8595cc84eeda2b9d182
nick.mateus@ecobee.com <zxcvbnm11Q!>
2019-08-26 16:50:05 -0400

    number injection on grid

--- sudoku_grid.py
+++ sudoku_grid.py
@@ -7,44 +7,83 @@ class sud_grid():
             raise ValueError
         else:
             self.div = int(div)
-            self.size = int(self.div**2)
-            print(f'{self.size} x {self.size} grid created!')
-            self.vertical_spacing = 1
-            self.horizontal_spacing = self.vertical_spacing * self.div * 3
+            self.nums_per_line = self.div**2
+            self.vertical_spacing = seperation
+            self.number_spacing = self.vertical_spacing * 2
+            self.horizontal_spacing = self.vertical_spacing * self.div * 3 + self.number_spacing
+            print(f'{self.nums_per_line} x {self.nums_per_line} grid created!')
+
+            self.grid = iter([1,2,3,4,5,6,7,8,9]*80)
     
     def draw_grid(self):
-        for i in range(self.div):
+        for _ in range(self.div):
             print(self.horizintal_seperator())
-            for j in range(self.div*self.vertical_spacing):
-                print(self.vertical_seperator())
+            for _ in range(self.div*self.vertical_spacing):
+                print(self.inject_grid_row())
         print(self.horizintal_seperator())
 
     def horizintal_seperator(self):
         h_segment = f'+{"-"*self.horizontal_spacing}'*self.div + '+'
         return h_segment
     
-    def vertical_seperator(self):
-        v_segment = f'|{" "*self.horizontal_spacing}'*self.div + '|'
-        return v_segment
+    def inject_grid_row(self):
+        row = ['|'] + ([" "]*self.horizontal_spacing + ['|']) * self.div
+        for i in range(0, ((self.number_spacing*(self.div+1)+self.div)*self.div)+(self.div+1), self.number_spacing + 1):
+            if row[i] is not ' ':
+                i -= 1
+                continue
+            row[i] = str(next(self.grid))
 
-s = sud_grid(4)
+        return ''.join(row)
+
+s = sud_grid()
 s.draw_grid()
 """
-+---------+---------+---------+
-| 1  2  3 |         |         |
-| 3  1  6 |         |         |
-| 1  2  3 |         |         |
-+---------+---------+---------+
++-----------+---------+---------+
+|  1  2  3  |         |         |
+|  1  2  3  |         |         |
+|  1  2  3  |         |         |
++-----------+---------+---------+
 | 1  2  3 |         |         |
 | 3  1  6 |         |         |
 
 
 +------------+------------+------------+------------+
 | 2  6  3  4 |            |            |            |
-|            |            |            |            |
-|            |            |            |            |
-|            |            |            |            |
+| 2  6  3  4 |            |            |            |
+| 2  6  3  4 |            |            |            |
+| 2  6  3  4 |            |            |            |
 +------------+------------+------------+------------+
 |            |            |            |            |
+
++--------------+--------------+--------------+--------------+
+|  2  6  3  4  |              |              |              |
+|  2  6  3  4  |              |              |              |
+|  2  6  3  4  |              |              |              |
+|  2  6  3  4  |              |              |              |
++--------------+--------------+--------------+--------------+
+
++-----------------+-----------------+-----------------+
+|   2    3    4   |                 |                 |
+|                 |                 |                 |
+|                 |                 |                 |
+|                 |                 |                 |
+|                 |                 |                 |
+|                 |                 |                 |
++-----------------+-----------------+-----------------+
+|                 |                 |                 |
+|                 |                 |                 |
+|                 |                 |                 |
+|                 |                 |                 |
+|                 |                 |                 |
+|                 |                 |                 |
++-----------------+-----------------+-----------------+
+|                 |                 |                 |
+|                 |                 |                 |
+|                 |                 |                 |
+|                 |                 |                 |
+|                 |                 |                 |
+|                 |                 |                 |
++-----------------+-----------------+-----------------+
 
 """
