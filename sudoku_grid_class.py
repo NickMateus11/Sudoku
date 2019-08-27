a9e456cc2c31ce635e929a8a526976f6f26f0334
nick.mateus@ecobee.com <zxcvbnm11Q!>
2019-08-27 12:19:42 -0400

    improved formating for grid to group class

--- sudoku_grid_class.py
+++ sudoku_grid_class.py
@@ -1,15 +1,51 @@
 import cell_group_class
-
+import math
 class grid:
     def __init__(self, dim, init_vals=None):
         if dim <= 0:
             raise ValueError
         self.dim = dim
-        self.groups = [cell_group_class.cell_group(self.dim, init_vals)] * self.dim
+        self.init_vals = init_vals
+        if init_vals is None:
+            self.formatted_init_vals = [[None]*(self.dim**2)]*(self.dim**2)
+        else:
+            self.formatted_init_vals = [list() for _ in range(self.dim**2)]
+            for i in range(len(init_vals)):
+                index = (i//self.dim)%self.dim + self.dim*(i//(self.dim**3))
+                self.formatted_init_vals[index].append(init_vals[i])
 
-    def linearize_cell_nums(self):
-        val_list = []
-        for group in self.groups:
-            val_list += group.list_cell_nums()
-        return val_list
+    def get_all_cell_nums(self):
+        return self.init_vals
+
+    def get_cell_nums_rows(self):
+        pass
+
+a = [1,2,3,4,5,6,7,8,9] * 9
+g = grid(3, a)
+
+# a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16] * 16
+# g = grid(4, a)
+'''
+[1 2 3  4 5 6  7 8 9 
+ 1 2 3  4 5 6  7 8 9 
+ 1 2 3  4 5 6  7 8 9 
+ 
+ 1 2 3  4 5 6  7 8 9 
+ 1 2 3  4 5 6  7 8 9 
+ 1 2 3  4 5 6  7 8 9 
+
+ 1 2 3  4 5 6  7 8 9 
+ 1 2 3  4 5 6  7 8 9 
+ 1 2 3  4 5 6  7 8 9]
+
+[[1,2,3,1,2,3,1,2,3],[4,5,6,4,5,6,4,5,6],[7,8,9,7,8,9,7,8,9],
+ [1,2,3,1,2,3,1,2,3],[4,5,6,4,5,6,4,5,6],[7,8,9,7,8,9,7,8,9],
+ [1,2,3,1,2,3,1,2,3],[4,5,6,4,5,6,4,5,6],[7,8,9,7,8,9,7,8,9]]
+
+
+[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+
+
+'''
 
